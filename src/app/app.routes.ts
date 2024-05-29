1d88a7418dac03cc4aeb99326e7e98735f220044
lFerDanl <fcossio100@gmail.com>
2024-05-29 11:58:51 -0400

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './autenticacion/login/login.component';
+import { RegisterComponent } from './autenticacion/register/register.component';
+import { ProfileComponent } from './gestionUsuario/profile/profile.component';
+import { UpdateuserComponent } from './gestionUsuario/updateuser/updateuser.component';
+import { UserslistComponent } from './gestionUsuario/userslist/userslist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'registre', component: RegisterComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'update/:id', component: UpdateuserComponent},
+    {path: 'users', component: UserslistComponent},
+    {path: '**', component: LoginComponent},
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+
+];
